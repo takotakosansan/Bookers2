337ea42b04c7a998d545cc0b8c3634ad5d5f63db
EC2 Default User <ec2-user@ip-172-31-41-217.ec2.internal>
2022-05-25 09:56:43 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+   before_action :correct_user, only: [:edit, :update]
+   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    @user = current_user
+    if @book.save
+    redirect_to book_path(@book.id), notice:'You have created book successfully.' 
+    else
+    render :index
+    end
+   end
+
+  def index
+    @user = current_user
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @bookss = Book.new
+    @users = @book.user
+  end
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+    else
+        redirect_to book_path(@book.id)
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+
+      redirect_to book_path(@book.id), notice:'You have updated book successfully.' 
+
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :books, only: [:new, :index, :show, :update,:edit, :create, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
